6fffa13eab3a2c449c57f3159f820e9c99f03c96
Brian Anglin <brianranglin+gh@gmail.com>
2017-10-27 16:56:07 -0700

    Bump

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 var autodeploy = require('glitch-cd')
 app.use(autodeploy())
 
-let name = ':wave: Brian Anglin hi hi'
+let name = ':wave: Brian Anglin hi h 23224j209093i'
 
 app.get('/', (req, res, next) => {
   res.json({ status: 'ok', name })
