6a9ebb01cdcc3353187d38f2183feb9c1f35bca1
Brian Anglin <brianranglin+gh@gmail.com>
2017-10-27 16:35:02 -0700

    Intial commit w Brian as name

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// init project
+const express = require('express')
+const app = express()
+
+// Register our autodeployment hook
+var autodeploy = require('glitch-cd')
+app.use(autodeploy())
+
+let name = 'Brian'
+
+app.get('/', (req, res, next) => {
+  res.json({ status: 'ok', name: 'Brian' })
+})
+
+
+// listen for requests :)
+let listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
